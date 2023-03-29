b279d7a11c5c510ce679ce3a0127bd19592d666a
zaksalman <jojozon@naver.com>
2023-03-29 09:59:27 +0900

    첫번째 컴밋

---
+++ src/main/java/com/ha/JenkisTest2Application.java
@@ -0,0 +1,13 @@
+package com.ha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkisTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ha/JenkisTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkisTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
